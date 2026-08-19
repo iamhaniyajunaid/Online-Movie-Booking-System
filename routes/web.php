bae6e32e3c83c162ca4b6685ba6f23a7718d3c3a
iamhaniyajunaid <iamhaniyajunaid@gmail.com>
2026-08-19 14:18:11 -0700

    your message here

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,12 @@ Route::get('/', function () {
 Route::get('/user-home', function () {
     return view('user.index');
 });
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
